34cb7b8c838e7bcae3248d8dffbabdbbaa89cd79
nicejs-is-cool <nicejsalt@gmail.com>
2026-08-26 21:19:41 -0300

    functioning exporter

--- addons/godot_pmdcollab/axp.gd
+++ addons/godot_pmdcollab/axp.gd
@@ -3,13 +3,49 @@ extends RefCounted
 class_name AXP
 
 const _execFrames = ["HitFrame", "HurtFrame", "ReturnFrame", "RushFrame"]
-static var _execFramesCamelCase = _execFrames.map(func(x): return x.to_camel_case())
+const _execFramesCamelCase = ["hitFrame", "hurtFrame", "returnFrame", "rushFrame"]
 const _execFrameMapping = {
 	"hitFrame": "_on_hit_frame",
 	"hurtFrame": "_on_hurt_frame",
 	"returnFrame": "_on_return_frame",
 	"rushFrame": "_on_rush_frame"
 }
+
+static func xml_to_dictionary(xml_string: String) -> Dictionary:
+	var parser: XMLParser = XMLParser.new()
+	var error = parser.open_buffer(xml_string.to_utf8_buffer())
+	if error != OK:
+		push_error("Failed to parse XML string. Error code: ", error)
+		return {}
+	var root: Dictionary = {"name": "root", "children": []}
+	var node_stack: Array = [root]
+	
+	while parser.read() == OK:
+		var node_type = parser.get_node_type()
+		match node_type:
+			XMLParser.NODE_ELEMENT:
+				var element_name = parser.get_node_name()
+				var attributes = {}
+				for i in range(parser.get_attribute_count()):
+					attributes[parser.get_attribute_name(i)] = parser.get_attribute_value(i)
+				var new_node = {
+					"name": element_name,
+					"attributes": attributes,
+					"children": [],
+					"content": ""
+				}
+				node_stack[-1]["children"].append(new_node)
+				if not parser.is_empty():
+					node_stack.append(new_node)
+			XMLParser.NODE_ELEMENT_END:
+				if node_stack.size() > 1:
+					node_stack.pop_back()
+			XMLParser.NODE_TEXT:
+				var text = parser.get_node_data().strip_edges()
+				if not text.is_empty():
+					node_stack[-1]["content"] = text
+	return root["children"][0] if not root["children"].is_empty() else {}
+
 
 static func find_child_node(node, cname: String):
 	for child in node.children:
@@ -34,7 +70,7 @@ static func anim_to_dict(anim): # still missing properties but fuck it we ball
 	if durationsNode != null:
 		for durationNode in durationsNode.children:
 			if durationNode.name != "Duration":
-				print("unexpected node: {}", durationNode.name)
+				printerr("unexpected node: {} during parsing durations", durationNode.name)
 				continue
 			durations.push_back(int(durationNode.content))
 		result.durations = durations
@@ -57,11 +93,11 @@ static func anim_to_dict(anim): # still missing properties but fuck it we ball
 	return result
 static func load_anxml(path: String):
 	var xstr: String = FileAccess.get_file_as_string(path)
-	var dict = XmlHelper.xml_to_dictionary(xstr)
+	var dict = xml_to_dictionary(xstr)
 	# name, attributes, children, content
 	var animationsNode = find_child_node(dict, "Anims")
 	if animationsNode == null:
-		printerr("Couldn't find child node Anims")
+		printerr("Couldn't find child node Anims for importing SpriteCollab Animation Data, aborting")
 		return null
 	var animations: Array[Dictionary] = []
 	for animation in animationsNode.children:
@@ -77,7 +113,7 @@ static func _has_any_inside(arr1: Array, arr2: Array):
 	return false
 static func animation_to_gd(anim: Dictionary):
 	var animation: Animation = Animation.new()
-	print(anim.durations)
+	#print(anim.durations)
 	var totalLen = anim.durations.reduce(func(a, b): return a+b, 0)
 	var lengthInSeconds = float(totalLen) / 30 # here we assume EoS is running in 30fps
 	#print(totalLen)
@@ -91,16 +127,16 @@ static func animation_to_gd(anim: Dictionary):
 	animation.track_set_interpolation_type(track_idx, Animation.INTERPOLATION_NEAREST)
 	animation.track_set_path(track_idx, ".:frame_coords:x")
 	
-	var timeElapsed = (float(anim.durations[0])/totalLen)*lengthInSeconds
+	var timeElapsed = 0 #(float(anim.durations[0])/totalLen)*lengthInSeconds
 	var frames = 0
 	for duration in anim.durations:
 		var timeTaken = (float(duration) / totalLen) * lengthInSeconds
-		print("duration: %d; time taken: %f; elapsed: %f; frames: %d" % [duration, timeTaken, timeElapsed, frames])
+		#print("duration: %d; time taken: %f; elapsed: %f; frames: %d" % [duration, timeTaken, timeElapsed, frames])
 		timeElapsed += timeTaken
 		var key_idx = animation.track_insert_key(track_idx, timeElapsed, frames)
 		for execFrame in _execFramesCamelCase:
-			if anim.has(execFrame) && anim.get(execFrame) == frames:
-				print("adding exec frame %s" % execFrame)
+			if anim.has(execFrame) && anim.get(execFrame) == frames: # probably horribly inefficient but eh
+				#print("adding exec frame %s" % execFrame)
 				animation.track_insert_key(exec_track, timeElapsed, {
 					"method": _execFrameMapping[execFrame],
 					"args": []
--- src/scripts/main_scene.gd
+++ src/scripts/main_scene.gd
@@ -1,6 +1,7 @@
 extends Node3D
 @onready var strike_anim: Sprite3D = $StrikeAnim
 @onready var animation_player_3: AnimationPlayer = $AnimationPlayer3
+var animlib = preload("res://src/assets/pokemon/0001/AnimData.xml")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +9,7 @@ func _ready() -> void:
 	#var resp = axp.parse("res://src/assets/AnimData.xml")
 	#print(resp.shadowSize)
 	#print(resp.animations[1].index)
-	var anims = AXP.load_anxml("res://src/assets/pokemon/0001/AnimData.xml")
+	"""var anims = AXP.load_anxml("res://src/assets/pokemon/0001/AnimData.xml")
 	var lib = AnimationLibrary.new()
 	#var anim = animation_to_gd(anims.animations[0])
 	var walkAnim = AXP.get_animation(anims, &"Walk")
@@ -23,7 +24,10 @@ func _ready() -> void:
 	$AnimationPlayer2.add_animation_library("movement", lib)
 	$AnimationPlayer2.play(&"movement/Attack")
 	animation_player_3.add_animation_library("movement", lib)
-	animation_player_3.play(&"movement/Strike")
+	animation_player_3.play(&"movement/Strike")"""
+	$AnimationPlayer.play("AnimData/Walk")
+	print($AnimationPlayer.get_animation_library("AnimData").get_animation_data(&"Walk"))
+	
 var frame = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
